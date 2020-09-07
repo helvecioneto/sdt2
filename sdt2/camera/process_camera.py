c6a5d9e8fb02fe32a4735c93daf79c1a0ec63bbc
Helvecio Leal <helvecio.neto@inpe.br>
2020-09-07 09:27:36 -0300

    Reindex by 5min interval

--- sdt2/camera/process_camera.py
+++ sdt2/camera/process_camera.py
@@ -5,7 +5,6 @@ from modules.load_config import load_config
 import errno
 import os
 
-
 header = load_config()[0]['SKY_CAMERA']
 
 
@@ -23,8 +22,7 @@ def process_files(path):
     df['sigla'] = path[-3:]
     df['timestamp'] = df[0].astype(str) + df[1].astype(str)
     df['timestamp'] = pd.to_datetime(df['timestamp'] , format='%Y%m%d%H%M%S')
-    
-    
+
     df['year'] = df['timestamp'].dt.year
     df['day'] = df['timestamp'].dt.dayofyear
     df['min'] = df['timestamp'].dt.minute
@@ -39,12 +37,13 @@ def process_files(path):
     df = df.drop(columns=[0,1,2,3,4,5,6,7])
     mux = pd.MultiIndex.from_tuples(headers)
     df.columns = mux
-    
+  
     groups = df.groupby(pd.Grouper(key='timestamp', freq='M'))
     
     dfs = [group for _,group in groups]
     
     for i in dfs:
+
         output_path = load_config()[0]['OUTPUT'] + path[-3:] + '/Cobertura_Nuvens/' + str(i['timestamp'].dt.year.unique()[0]) + '/'
         output_file = path[-3:]+'_'+str(i['timestamp'].dt.year.unique()[0])+'_'+str(i['timestamp'].dt.strftime('%m').unique()[0])+'_CN_formatado.csv'
 
@@ -58,6 +57,15 @@ def process_files(path):
         
         output = output_path + output_file
         
+        i = i.set_index(['timestamp'])
+        i = i.resample('5min').first()
+        i = i.reset_index()
+        
+        tsmp = i['timestamp']
+        i.drop(labels=['timestamp'], axis=1, inplace = True)
+        i.insert(1, 'timestamp', tsmp)
+        i.dropna(inplace=True)
+
         os.system('cls' if os.name == 'nt' else 'clear')
         print(i)
         input()
