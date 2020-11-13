e433034fbcf3677fc7af7e943195606155871958
Helvecio Neto <helvecio.neto@inpe.br>
2020-11-13 16:08:18 -0300

    Find non-existent sensor and lost data

--- sdt2/modules/historical_generator.py
+++ sdt2/modules/historical_generator.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from modules.top_header import top_header
 from modules.load_config import load_config
-
+from datetime import datetime
 from dependecies import *
 
 def historic_generate():
@@ -165,74 +165,133 @@ def historic_generate():
         for i in range(len(head1)):
             mux.append([str(head1[i]).lower(),str(head2[i]).lower()])
         mux = pd.MultiIndex.from_tuples(mux)
+
+         ## GET ID OF STATION
+        id_st = locked_df_chk[2].values[0]
         
-        ##REINDX TO DETECT
-        try:
-            locked_df_chk = locked_df_chk.set_index(0).asfreq(freqc)
-            
-            if len(locked_df_chk) == len(final_df):
-                print('Aquivos iguais')
-                print(final_df)
-            else:
-                print('Arquivo reprovado')
-                print('Total de linhas vazias :',locked_df_chk[1].isna().sum())
-                emptyrows = locked_df_chk[1].index[locked_df_chk[1].apply(np.isnan)]
-                # print('Linhas vazias: ',emptyrows)
-                
-               
-                
-                ## SAVE PROCESS
-                output_dir = load_config()[0]['HISTORICAL_OUT'] + str(operational_stations[ans_file]).upper() + '/' + str(selected_year) + '/'
-                output_file_name = str(operational_stations[ans_file]).upper()  + '_' + str(selected_year) + '_' + str(locked_df_chk.index[0].strftime('%j')) + '_a_' + str(locked_df_chk.index[-1].strftime('%j')) + '_' +  str(dataTypes[ans_type]) + '.dat'
-                
-                ## CREATE DIR
-                pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
-                ## CREATE VERSION DIRS
-                pathlib.Path(output_dir+'/versions').mkdir(parents=True, exist_ok=True)
-                
-                 ## ADD COLUMNS
-                locked_df_chk = locked_df_chk.reset_index()
-                locked_df_chk.columns = mux
+        ## DETECT non-existent SENSOR
+        non_sens_col = []
+        object_type_c = locked_df_chk.select_dtypes(include=['object'])
+        for c in object_type_c:
+        	detc_mean = object_type_c[c].astype(float).mean()
+        	if str(detc_mean) == 'nan':
+        		non_sens_col.append(c)
 
-                file__ = output_dir + output_file_name
-                # ## CHEACK IF FILES EXISTS
-                if os.path.isfile(file__):
-                    warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + '  \nN. NULL ROWS:-> '+str(len(emptyrows))+'\nNULL ROWS:\n'+str(emptyrows) +'\n')
-                    logging.warning(warningmsg)
-                    print(locked_df_chk)
+        ## Generate all month days to compare
+        year_month = locked_df_chk[0].dt.strftime('%Y-%m').values[0]
+        month_generated = pd.date_range(
+                start = pd.Timestamp(year_month),                        
+                end = pd.Timestamp(year_month) + pd.offsets.MonthEnd(0),  # <-- 2018-08-31 with MonthEnd
+                freq = freqc
+            )
+
+        ## SET INDEX COLUMN OF TIME STAMP
+        locked_df_chk = locked_df_chk.set_index(0)
+
+        locked_df_chk = locked_df_chk.reindex(month_generated, fill_value=0)
+
+        ## ADD NON-EXISTENT VALUES
+        locked_df_chk[non_sens_col] = 5555
+        idx_values = locked_df_chk.loc[locked_df_chk[2] == 0].index
+
+        ## ADD YEAR
+        locked_df_chk.loc[idx_values, 3] = locked_df_chk.loc[idx_values].index.strftime('%Y').values
+        ## ADD JULIAN DAY
+        locked_df_chk.loc[idx_values, 4] = locked_df_chk.loc[idx_values].index.strftime('%j').values
+        ## ADD MINUTE
+        for hm in idx_values:
+        	s1 = hm.strftime('%d/%m/%Y 00:00')
+        	s2 = hm.strftime('%d/%m/%Y %H:%M')
+
+        	s1 = datetime.strptime(s1, '%d/%m/%Y %H:%M')
+        	s2 = datetime.strptime(s2, '%d/%m/%Y %H:%M')
+
+        	differenc = s2-s1
+        	minutes = divmod(differenc.seconds, 60) 
+        	locked_df_chk.loc[hm, 5] = minutes[0]
+
+        
+        locked_df_chk[2] = id_st
+
+        ## DATETIME COLUMNS + NON_SENSOR
+        dt_non_se = []
+        dt_non_se.append(2)
+        dt_non_se.append(3)
+        dt_non_se.append(4)
+        dt_non_se = dt_non_se + non_sens_col
+
+        ### ADD VALUES 3333,
+        diff_columns = np.setdiff1d(locked_df_chk.columns.values,dt_non_se)
+        locked_df_chk.loc[idx_values, diff_columns] = 3333
+
+        locked_df_chk = locked_df_chk.reset_index()
+        locked_df_chk.columns = mux
+        print(locked_df_chk.tail(50))
+
+
+        # ##REINDX TO DETECT
+        # try:
+        #     locked_df_chk = locked_df_chk.set_index(0).asfreq(freqc)
+            
+        #     if len(locked_df_chk) == len(final_df):
+        #         print('Aquivos iguais')
+        #         print(final_df)
+        #     else:
+        #         print('Arquivo reprovado')
+        #         print('Total de linhas vazias :',locked_df_chk[1].isna().sum())
+        #         emptyrows = locked_df_chk[1].index[locked_df_chk[1].apply(np.isnan)]
+        #         # print('Linhas vazias: ',emptyrows)
+                
+        #         ## SAVE PROCESS
+        #         output_dir = load_config()[0]['HISTORICAL_OUT'] + str(operational_stations[ans_file]).upper() + '/' + str(selected_year) + '/'
+        #         output_file_name = str(operational_stations[ans_file]).upper()  + '_' + str(selected_year) + '_' + str(locked_df_chk.index[0].strftime('%j')) + '_a_' + str(locked_df_chk.index[-1].strftime('%j')) + '_' +  str(dataTypes[ans_type]) + '.dat'
+                
+        #         ## CREATE DIR
+        #         pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+        #         ## CREATE VERSION DIRS
+        #         pathlib.Path(output_dir+'/versions').mkdir(parents=True, exist_ok=True)
+                
+        #           ## ADD COLUMNS
+        #         locked_df_chk = locked_df_chk.reset_index()
+        #         locked_df_chk.columns = mux
+
+        #         file__ = output_dir + output_file_name
+        #         # ## CHEACK IF FILES EXISTS
+        #         if os.path.isfile(file__):
+        #             warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + '  \nN. NULL ROWS:-> '+str(len(emptyrows))+'\nNULL ROWS:\n'+str(emptyrows) +'\n')
+        #             logging.warning(warningmsg)
+        #             print(locked_df_chk.head(50))
                     
-                    ## CHECK LAST VERSION
-                    if len(os.listdir(output_dir+'/versions') ) == 0:
-                        shutil.move(file__, output_dir+'/versions/'+output_file_name+'.v01')
-                        ## CREATE FILE
-                        locked_df_chk.to_csv(file__,index=False)
+        #             ## CHECK LAST VERSION
+        #             if len(os.listdir(output_dir+'/versions') ) == 0:
+        #                 shutil.move(file__, output_dir+'/versions/'+output_file_name+'.v01')
+        #                 ## CREATE FILE
+        #                 locked_df_chk.to_csv(file__,index=False)
                         
-                        warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + ' File version(0)  \nN. NULL ROWS:-> '+str(len(emptyrows)) +'\n')
-                        logging.warning(warningmsg)
+        #                 warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + ' File version(0)  \nN. NULL ROWS:-> '+str(len(emptyrows)) +'\n')
+        #                 logging.warning(warningmsg)
 
-                    else:    
-                        versions = [fn for fn in listdir(output_dir+'/versions/') if not fn.startswith('.')]
-                        shutil.move(file__, output_dir+'/versions/'+output_file_name+'v0'+str((int(versions[-1][-2:]) + 1)))
-                        ## CREATE FILE
-                        locked_df_chk.to_csv(file__,index=False)
+        #             else:    
+        #                 versions = [fn for fn in listdir(output_dir+'/versions/') if not fn.startswith('.')]
+        #                 shutil.move(file__, output_dir+'/versions/'+output_file_name+'v0'+str((int(versions[-1][-2:]) + 1)))
+        #                 ## CREATE FILE
+        #                 locked_df_chk.to_csv(file__,index=False)
                         
-                        warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + ' File version('+ str(int(versions[-1][-2:]) + 1)+ ')  \nN. NULL ROWS:-> '+str(len(emptyrows)) +'\n')
-                        logging.warning(warningmsg)
+        #                 warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + ' File version('+ str(int(versions[-1][-2:]) + 1)+ ')  \nN. NULL ROWS:-> '+str(len(emptyrows)) +'\n')
+        #                 logging.warning(warningmsg)
 
-                else:
-                    warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + ' File version(0)  \nN. NULL ROWS:-> '+str(len(emptyrows)) +'\n')
-                    logging.warning(warningmsg)
-                    ## CREATE FILE
-                    print(locked_df_chk)
-                    locked_df_chk.to_csv(file__,index=False)
+        #         else:
+        #             warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + ' File version(0)  \nN. NULL ROWS:-> '+str(len(emptyrows)) +'\n')
+        #             logging.warning(warningmsg)
+        #             ## CREATE FILE
+        #             print(locked_df_chk)
+        #             locked_df_chk.to_csv(file__,index=False)
 
                 
-        except:
-            print('Arquivo reprovado')
-            locked_df_chk = locked_df_chk.set_index(0)
-            duplicated_lines = locked_df_chk[locked_df_chk.index.duplicated()]
-            print('Linhas duplicadas : ',duplicated_lines)
-            warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + '  \nN. DUPLICATED ROWS:-> '+str(len(duplicated_lines))+'\DUPLICATED ROWS:\n'+str(duplicated_lines) +'\n')
-            logging.warning(warningmsg)
-            
-       
+        # except:
+        #     print('Arquivo reprovado')
+        #     locked_df_chk = locked_df_chk.set_index(0)
+        #     duplicated_lines = locked_df_chk[locked_df_chk.index.duplicated()]
+        #     print('Linhas duplicadas : ',duplicated_lines)
+        #     warningmsg = ('\nSTATION-> '+str(output_file_name[:-4]) + '  \nN. DUPLICATED ROWS:-> '+str(len(duplicated_lines))+'\DUPLICATED ROWS:\n'+str(duplicated_lines) +'\n')
+        #     logging.warning(warningmsg)
